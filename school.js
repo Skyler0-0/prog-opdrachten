2fd87452db20064a792fb623c9d0fb9f298c4fad
Skyler0-0 <39634@ma-web.nl>
2024-09-25 16:18:04 +0200

    Add files via upload

---
+++ school.js
@@ -0,0 +1,9 @@
+var d = new Date();
+var huidigedag = d.getDay();
+
+
+if(huidigedag == 6 || huidigedag == 0 ){
+    console.log("Lekker uitslapen");
+} else {
+    console.log("Ik moet naar school");
+}
